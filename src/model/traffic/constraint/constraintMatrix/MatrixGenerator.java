7503ac84320699550520d47561761f088d8fbd1e
rineseM <rinesemorina@kodelabs.com>
2023-05-21 21:25:02 +0200

    added documentation to constraintMatrix

--- src/model/traffic/constraint/constraintMatrix/MatrixGenerator.java
+++ src/model/traffic/constraint/constraintMatrix/MatrixGenerator.java
@@ -10,31 +10,49 @@ public class MatrixGenerator {
     private static final IntersectionType[] VALUES = IntersectionType.values();
     private static final Random RANDOM = new Random();
 
+    /**
+     * Gjeneron një file CSV me matricë jo-smetrike të kufizimeve.
+     *
+     * @param fileName Emri i file CSV që do të gjenerohet.
+     * @param size     Madhësia e matricës (numri i rreshtave dhe kolonave).
+     */
     public static void generateNonSymmetricCSV(String fileName, int size) {
         try (FileWriter writer = new FileWriter(fileName)) {
             IntersectionType[][] matrix = new IntersectionType[size][size];
+
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < size; j++) {
                     IntersectionType value = VALUES[RANDOM.nextInt(VALUES.length)];
                     matrix[i][j] = value;
                 }
             }
+
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < size; j++) {
                     writer.write(matrix[i][j].toString());
+
                     if (j < size - 1) {
                         writer.write(",");
                     }
                 }
+
                 writer.write("\n");
             }
         } catch (IOException e) {
             System.out.println("Error writing to file: " + e.getMessage());
         }
     }
+
+    /**
+     * Gjeneron një file CSV me matricë simetrike të kufizimeve.
+     *
+     * @param fileName Emri i file CSV që do të gjenerohet.
+     * @param size     Madhësia e matricës (numri i rreshtave dhe kolonave).
+     */
     public static void generateSymmetricCSV(String fileName, int size) {
         try (FileWriter writer = new FileWriter(fileName)) {
             IntersectionType[][] matrix = new IntersectionType[size][size];
+
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j <= i; j++) {
                     IntersectionType value = VALUES[RANDOM.nextInt(VALUES.length)];
@@ -42,19 +60,23 @@ public class MatrixGenerator {
                     matrix[j][i] = value;
                 }
             }
+
             for (int i = 0; i < size; i++) {
                 for (int j = 0; j < size; j++) {
                     writer.write(matrix[i][j].toString());
+
                     if (j < size - 1) {
                         writer.write(",");
                     }
                 }
+
                 writer.write("\n");
             }
         } catch (IOException e) {
             System.out.println("Error writing to file: " + e.getMessage());
         }
     }
+
     public static void main(String[] args) {
 //        String fileName = "matrix.csv";
 //        int size = 16;
